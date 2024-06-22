e1c71cfccce4de02ea45b060ee834d3fa48e4377
Demo Robot <demo11website@gmail.com>
2024-06-22 15:39:18 +0500

    phone verification

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,17 @@ import "@fontsource/unbounded/600.css";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import client from "@/apollo-client";
+import Smartlook from "smartlook-client";
+import { useEffect } from "react";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
+  useEffect(() => {
+    Smartlook.init("91364c9de0632717bef2034465f9a83818940056");
+  }, []);
+
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
