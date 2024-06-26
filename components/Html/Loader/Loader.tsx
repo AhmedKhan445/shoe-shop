a2970d4c23e264ea7f8f0dc1c7b2a1ca30075e3c
Demo Robot <demo11website@gmail.com>
2024-06-26 10:09:45 +0500

    Update Loader.tsx

--- components/Html/Loader/Loader.tsx
+++ components/Html/Loader/Loader.tsx
@@ -4,25 +4,23 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 
 const Loader = () => {
-  // const [state, setState] = useState<number>(0);
+  const [state, setState] = useState<number>(0);
 
-  const { progress, errors } = useProgress();
+  const { progress } = useProgress();
 
-  console.log(errors, "error");
   console.log(Math.round(progress), "progress");
 
-  // useEffect(() => {
-  //   setState(Math.round(progress));
-  // }, [progress]);
+  useEffect(() => {
+    setState(Math.round(progress));
+  }, [progress]);
 
   return (
     <div data-hide={progress >= 100} className={s.main}>
       <Image src="/loader.png" height={300} width={300} alt="loader" />
       <h4>Please wait , good things take time to come</h4>
-      <progress value={Math.round(progress)} max={100} />
-      {/* <div className={s.progress}>
+      <div className={s.progress}>
         <div style={{ width: `${state}%` }} className={s.progress_bar} />
-      </div> */}
+      </div>
     </div>
   );
 };
