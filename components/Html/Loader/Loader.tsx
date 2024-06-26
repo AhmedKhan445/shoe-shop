341c781376c9d0c25e4a7b7dc5b831b22a7c7c51
Demo Robot <demo11website@gmail.com>
2024-06-26 15:12:12 +0500

    infinite loader

--- components/Html/Loader/Loader.tsx
+++ components/Html/Loader/Loader.tsx
@@ -1,25 +1,18 @@
 import { useProgress } from "@react-three/drei";
 import s from "./loader.module.scss";
-import { useEffect, useState } from "react";
 import Image from "next/image";
 
 const Loader = () => {
-  const [state, setState] = useState<number>(0);
-
   const { progress } = useProgress();
-
-  console.log(Math.round(progress), "progress");
-
-  useEffect(() => {
-    setState(Math.round(progress));
-  }, [progress]);
 
   return (
     <div data-hide={progress >= 100} className={s.main}>
       <Image src="/loader.png" height={300} width={300} alt="loader" />
       <h4>Please wait , good things take time to come</h4>
       <div className={s.progress}>
-        <div style={{ width: `${state}%` }} className={s.progress_bar} />
+        {[...Array(40)].map((e, i) => {
+          return <div key={i} className={s.progress_bar} />;
+        })}
       </div>
     </div>
   );
